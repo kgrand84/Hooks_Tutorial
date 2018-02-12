9f88ef3e6a692b42187796e97be25ac8b8a7f583
kgrand84 <k.grand@cpanel.com>
2018-02-12 12:59:08 -0600

    Update MyHook.pm
    Finished tutorial

--- MyHook.pm
+++ MyHook.pm
@@ -18,6 +18,7 @@ use JSON;
 my $logger = Cpanel::Logger->new();
 
 # Embed hook attributes alongside the action code.
+# This piece is the actual hook
 sub describe {
     my $hooks = [
         {
@@ -36,3 +37,38 @@ sub describe {
     ];
     return $hooks;
 }
+
+# Set the file to preserve and the temp file location.
+my $preserve = "/usr/local/cpanel/base/horde/turba/config/backends.php";
+my $temp = "/usr/local/cpanel/base/horde/turba/config/backends.php.temp";
+
+#Before upcp, copy the file to the temp file. 
+sub copyfile {
+  #Get the data that the system passes to the hook
+  my ( $context, $data ) = @_;
+  
+  #Add a log entry for debugging.
+  $logger->info("***** Copy my file before upcp *****");
+ 
+  # Copy my file.
+  copy($preserve,$temp) or die "Copy failed: $!";
+};
+
+# After upcp, copy the file back into place.
+sub replacefile {
+    # Get the data that the system passes to the hook.
+    my ( $context, $data ) = @_;
+ 
+    # Add a log entry for debugging.
+    $logger->info("***** Replace the temp file *****");
+ 
+    # Replace my file.
+    copy($temp,$preserve) or die "Replacement failed: $!";
+ 
+    # Add a log entry for debugging.
+    $logger->info("***** Delete the temp file *****");
+ 
+    # Delete the temp file to keep cruft off my system.
+    unlink($temp) or die "Cruft removal failed: $!";
+};
+
